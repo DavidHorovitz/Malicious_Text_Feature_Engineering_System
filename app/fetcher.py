4235f645ad474026179e02741f9e16f67e3f0612
david horovitz <dovidho@gmail.com>
2025-08-21 11:22:08 +0300

    conect to mongo

--- app/fetcher.py
+++ app/fetcher.py
@@ -1,5 +1,6 @@
 import pymongo
 import os
+import pandas as pd
 class MongoLoader:
     def __init__(self):
 
@@ -7,12 +8,22 @@ class MongoLoader:
         self.MONGO_USER = os.getenv("MONGO_USER", "IRGC")
         self.MONGO_PASS = os.getenv("MONGO_PASS", "iraniraniran")
         self.MONGO_DB = os.getenv("MONGO_DB", "IranMalDB")
-        self.connection_string=os.getenv("connection_string","mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/")
+
+        self.connection_string=os.getenv("connection_string",f"mongodb+srv://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_DB}.gurutam.mongodb.net/")
 
         self.myclient = pymongo.MongoClient(self.connection_string)
         self.mydb = self.myclient[self.MONGO_DB]
 
     def get_all_data(self):
 
-        mycol = self.mydb["enemy_soldiers"]
-        return list(mycol.find())
+        mycol = self.mydb["tweets"]
+        df=pd.DataFrame(list(mycol.find()))
+        return df
+
+# a=MongoLoader()
+# print(a.get_all_data())
+
+
+
+
+
---
+++ app/main.py
@@ -0,0 +1,9 @@
+# import pymongo
+#
+# connection_string = "mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/"
+# client = pymongo.MongoClient(connection_string)
+# db = client["IranMalDB"]
+# collection = db["tweets"]
+#
+# for doc in collection.find().limit(5):
+#     print(doc)
